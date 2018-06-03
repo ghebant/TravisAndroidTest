34a70a53db14e968c0ddd1d5af17c98d674e44e7
Travis CI <travis@travis-ci.org>
2018-06-03 11:39:53 +0200

    Travis build:

--- push_master.sh
+++ push_master.sh
@@ -11,12 +11,11 @@ commit_website_files() {
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
-#$TRAVIS_BRANCH
 upload_files() {
-  #git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
-  git push origin head:master
+  git remote add origin https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin master
 }
-#test
-#setup_git
+
+setup_git
 commit_website_files
 upload_files
