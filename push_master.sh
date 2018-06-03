b1a37d2044225a87a27bbb75a40056b1dcb77f67
Travis CI <travis@travis-ci.org>
2018-06-03 11:45:59 +0200

    Travis build:

--- push_master.sh
+++ push_master.sh
@@ -12,7 +12,7 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
+  git remote add origin https://${GH_TOKEN}@github.com/ghebant/TravisAndroidTest.git > /dev/null 2>&1
   git push --quiet --set-upstream origin master
 }
 
