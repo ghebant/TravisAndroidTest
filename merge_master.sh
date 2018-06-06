b8454468f2b3bd1384b37ba84c9da73071a367b6
ghebant <guillaume.hebant@epitech.eu>
2018-06-06 22:52:33 +0200

    test push

--- merge_master.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git config --global user.email "guillaume.hebant@epitech.eu"
-git config --global user.name "ghebant"
-#git remote add master_branch https://$1@github.com/ghebant/TravisAndroidTest.git #> /dev/null 2>&1
-git push https://$1@github.com/ghebant/TravisAndroidTest master
-#git push https://bc6bbc90201316ea45daded302f0f55917aad13d@github.com/ghebant/TravisAndroidTest HEAD
---
+++ push_master2.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout master
+  git add --all
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin https://${GH_TOKEN}@github.com/ghebant/TravisAndroidTest.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin master
+}
+
+merge() {
+  git checkout master
+  git merge branch_test
+  #git push https://${GH_TOKEN}@github.com/ghebant/TravisAndroidTest HEAD > /dev/null 2>&1
+}
+
+test2() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+  git remote add origin https://${GH_TOKEN}@github.com/ghebant/TravisAndroidTest.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin master
+}
+
+test2
+
+#merge
+#setup_git
+#commit_website_files
+#upload_files
+
+#TEST 3
